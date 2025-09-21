936e535e1bf0a0cd3f8040dadf7ca523d7e9798e
hwyao <haowen.yao@tum.de>
2025-09-21 02:39:26 +0000

    Deploying to main from @ hwyao/FLIQC_controller_core@5317de1376141fffc08095a18fea597039c05deb 🚀

--- FLIQC_controller_core/search/functions_0.js
+++ FLIQC_controller_core/search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['fliqc_5fcontroller_5fjoint_5fvelocity_5fbasic_0',['FLIQC_controller_joint_velocity_basic',['../class_f_l_i_q_c__controller__core_1_1_f_l_i_q_c__controller__joint__velocity__basic.html#abf58cd6786d276964034add8156ef35c',1,'FLIQC_controller_core::FLIQC_controller_joint_velocity_basic']]],
+  ['fliqc_5fcontroller_5fjoint_5fvelocity_5fbasic_0',['fliqc_controller_joint_velocity_basic',['../class_f_l_i_q_c__controller__core_1_1_f_l_i_q_c__controller__joint__velocity__basic.html#abf58cd6786d276964034add8156ef35c',1,'FLIQC_controller_core::FLIQC_controller_joint_velocity_basic::FLIQC_controller_joint_velocity_basic(int dim_q)'],['../class_f_l_i_q_c__controller__core_1_1_f_l_i_q_c__controller__joint__velocity__basic.html#a5bcc20c05a32b5a1edda59b06196dee8',1,'FLIQC_controller_core::FLIQC_controller_joint_velocity_basic::FLIQC_controller_joint_velocity_basic(const FLIQC_controller_joint_velocity_basic &amp;other)']]],
   ['formatdoublevector_1',['formatDoubleVector',['../class_f_l_i_q_c__controller__core_1_1_l_c_q_pow_exception.html#a6d65979c2b0fdee44aecf8b0937fbea1',1,'FLIQC_controller_core::LCQPowException']]],
   ['formatmatrix_2',['formatMatrix',['../class_f_l_i_q_c__controller__core_1_1_l_c_q_pow_exception.html#adb05e3499d5d2d1829c3300c61086882',1,'FLIQC_controller_core::LCQPowException']]],
   ['formatvector_3',['formatVector',['../class_f_l_i_q_c__controller__core_1_1_l_c_q_pow_exception.html#a1c24905b50ce98c994ea48c240a6ca98',1,'FLIQC_controller_core::LCQPowException']]]
